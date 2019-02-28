57214df34f7ad8c2c2dda72296eeab3b39ab5f96
Nelma Barros <nbarros@uolinc.com>
2019-02-28 16:57:16 -0300

    Aula 1

---
+++ app/src/main/java/com/example/estudoandroid/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.estudoandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView tvPrincipal = findViewById(R.id.tvPrincipal);
+        final EditText etPrincipal = findViewById(R.id.etPrincipal);
+        final Button btEnviar = findViewById(R.id.btEnviar);
+
+        btEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvPrincipal.setText(etPrincipal.getText().toString());
+            }
+        });
+
+    }
+}
