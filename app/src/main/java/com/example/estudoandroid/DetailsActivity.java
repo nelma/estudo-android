e34fb7b6d6cdc58b1cd41598076c2913b19b3d52
Nelma Barros <nbarros@uolinc.com>
2019-02-28 19:48:50 -0300

    exercicio form de usr

--- app/src/main/java/com/example/estudoandroid/DetailsActivity.java
+++ app/src/main/java/com/example/estudoandroid/DetailsActivity.java
@@ -13,15 +13,16 @@ import android.widget.Toast;
 
 public class DetailsActivity extends AppCompatActivity {
 
-    static final String TEXT = "TEXT";
+    static final String NOME = "NOME";
+    static final String SOBRENOME = "SOBRENOME";
+    static final String EMAIL = "EMAIL";
+    static final String PASSWORD = "PASSWORD";
 
-    private TextView textView;
-    private EditText editText;
-    private Button btApplyChanges;
-    private Button btKeepValue;
-    private Button btResetValue;
-
-    private String persistText;
+    private TextView txtNome;
+    private TextView txtSobrenome;
+    private TextView txtEmail;
+    private TextView txtPassword;
+    private Button btSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,60 +34,84 @@ public class DetailsActivity extends AppCompatActivity {
 
         if(intent != null) {
             String info = intent.getStringExtra("INFO");
-            Toast.makeText(this, info, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "info", Toast.LENGTH_LONG).show();
         }
 
-        textView = findViewById(R.id.textView);
-        editText = findViewById(R.id.editText);
-        btApplyChanges = findViewById(R.id.btApplyChanges);
-        btKeepValue = findViewById(R.id.btKeepValue);
-        btResetValue = findViewById(R.id.btResetValue);
+        txtNome = findViewById(R.id.edNome);
+        txtSobrenome = findViewById(R.id.edSobrenome);
+        txtEmail = findViewById(R.id.edEmail);
+        txtPassword = findViewById(R.id.edPassword);
+        btSend = findViewById(R.id.btSend);
 
-        btApplyChanges.setOnClickListener(new View.OnClickListener() {
+        btSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String text = editText.getText().toString();
-                textView.setText((text));
 
-                //Definindp resultado
-                if(intent != null) {
-                    intent.putExtra("RESULTADO", text);
-                    setResult(RESULT_OK, intent);
-                    DetailsActivity.this.finish();
+
+                if(txtNome.getText().toString().isEmpty() || txtSobrenome.getText().toString().isEmpty()
+                        || txtEmail.getText().toString().isEmpty() || txtPassword.getText().toString().isEmpty()) {
+                    Toast.makeText(DetailsActivity.this, "Todos os campos são obigatórios", Toast.LENGTH_LONG).show();
+                } else {
+
+                    if (intent != null) {
+                        intent.putExtra("NOME", txtNome.getText().toString());
+                        intent.putExtra("SOBRENOME", txtSobrenome.getText().toString());
+                        intent.putExtra("EMAIL", txtEmail.getText().toString());
+                        intent.putExtra("SENHA", txtPassword.getText().toString());
+                        setResult(RESULT_OK, intent);
+                        DetailsActivity.this.finish();
+                    }
                 }
             }
         });
 
-        btKeepValue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                persistText = editText.getText().toString();
-                Toast.makeText(DetailsActivity.this, "Persisted Value: " + persistText, Toast.LENGTH_LONG).show();
-            }
-        });
-
-        btResetValue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                persistText = null;
-                Toast.makeText(DetailsActivity.this, "Value reseted", Toast.LENGTH_LONG).show();
-            }
-        });
+//        textView = findViewById(R.id.textView);
+//        editText = findViewById(R.id.editText);
+//        btApplyChanges = findViewById(R.id.btApplyChanges);
+//        btKeepValue = findViewById(R.id.btKeepValue);
+//        btResetValue = findViewById(R.id.btResetValue);
+//
+//        btApplyChanges.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String text = editText.getText().toString();
+//                textView.setText((text));
+//
+//                //Definindp resultado
+//                if(intent != null) {
+//                    intent.putExtra("RESULTADO", text);
+//                    setResult(RESULT_OK, intent);
+//                    DetailsActivity.this.finish();
+//                }
+//            }
+//        });
+//
+//        btKeepValue.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                persistText = editText.getText().toString();
+//                Toast.makeText(DetailsActivity.this, "Persisted Value: " + persistText, Toast.LENGTH_LONG).show();
+//            }
+//        });
+//
+//        btResetValue.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                persistText = null;
+//                Toast.makeText(DetailsActivity.this, "Value reseted", Toast.LENGTH_LONG).show();
+//            }
+//        });
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        if(persistText != null && !persistText.isEmpty()) {
-            outState.putString(TEXT, textView.getText().toString());
-        } else {
-            outState.remove(TEXT);
-        }
+        outState.putString(NOME, txtNome.getText().toString());
+        outState.putString(SOBRENOME, txtSobrenome.getText().toString());
+        outState.putString(EMAIL, txtEmail.getText().toString());
+        outState.putString(PASSWORD, txtPassword.getText().toString());
 
-        //Exemplo de Serialização
-        UsuarioSerializable usuarioSerializable = new UsuarioSerializable("Nelmaaaaaaaa", "Sobrenome");
-        outState.putSerializable("USUARIO_SERIALIZABLE", usuarioSerializable);
-
-        UsuarioParcelable usuarioParcelable =  new UsuarioParcelable("Parcelableeeee", "Sobrenome");
+        UsuarioParcelable usuarioParcelable =
+                new UsuarioParcelable(txtNome.getText().toString(), txtSobrenome.getText().toString());
         outState.putParcelable("USUARIO_PARCELABLE", usuarioParcelable);
 
         super.onSaveInstanceState(outState);
@@ -96,16 +121,16 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
 
-        String text = savedInstanceState.getString(TEXT);
-        textView.setText(text);
+        String nome = savedInstanceState.getString(NOME);
+        String sobrenome = savedInstanceState.getString(SOBRENOME);
+        String email = savedInstanceState.getString(EMAIL);
+        String password = savedInstanceState.getString(PASSWORD);
 
-        UsuarioSerializable usuarioSerializable =
-                (UsuarioSerializable) savedInstanceState.getSerializable("USUARIO_SERIALIZABLE");
 
-        if(usuarioSerializable != null) {
-            Log.d(getClass().getName(), usuarioSerializable.getNome());
-            Log.d(getClass().getName(), usuarioSerializable.getSobrenome());
-        }
+        txtNome.setText(nome);
+        txtSobrenome.setText(sobrenome);
+        txtEmail.setText(email);
+        txtPassword.setText(password);
 
         UsuarioParcelable usuarioParcelable = savedInstanceState.getParcelable("USUARIO_PARCELABLE");
         if (usuarioParcelable != null) {
--- app/src/main/java/com/example/estudoandroid/MainActivity.java
+++ app/src/main/java/com/example/estudoandroid/MainActivity.java
@@ -6,14 +6,40 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_CODE = 1;
+
+    private TextView txtNome;
+    private TextView txtSobrenome;
+    private TextView txtEmail;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        String nome = data.getStringExtra("NOME");
+        String sobrenome = data.getStringExtra("SOBRENOME");
+        String email = data.getStringExtra("EMAIL");
+
+        Toast.makeText(this, "Voltei", Toast.LENGTH_LONG).show();
+
+        txtNome = findViewById(R.id.txtNome);
+        txtSobrenome = findViewById(R.id.txtSobrenome);
+        txtEmail = findViewById(R.id.txtEmail);
+
+        txtNome.setText(nome);
+        txtSobrenome.setText(sobrenome);
+        txtEmail.setText(email);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
