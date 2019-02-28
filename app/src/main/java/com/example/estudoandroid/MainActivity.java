e1f5f985bc3fb1e6bbf478db5879ddaad98b2d7f
Nelma Barros <nbarros@uolinc.com>
2019-02-28 18:35:13 -0300

    Aula 2

--- app/src/main/java/com/example/estudoandroid/MainActivity.java
+++ app/src/main/java/com/example/estudoandroid/MainActivity.java
@@ -1,30 +1,46 @@
 package com.example.estudoandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        final TextView tvPrincipal = findViewById(R.id.tvPrincipal);
-        final EditText etPrincipal = findViewById(R.id.etPrincipal);
-        final Button btEnviar = findViewById(R.id.btEnviar);
-
-        btEnviar.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                tvPrincipal.setText(etPrincipal.getText().toString());
+            public void onClick(View view) {
+
+                // Inicia uma Activity, enviando uma informação junto
+//                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+//                intent.putExtra("INFO", "MainActivity");
+//                startActivity(intent);
+
+                // Inicia uma Activity, esperando uma resposta/resultado
+                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+
+
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
-
     }
+
 }
