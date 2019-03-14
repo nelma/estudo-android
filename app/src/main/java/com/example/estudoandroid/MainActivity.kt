c3d050bf215ac03148662c13046d33bdca952982
Nelma Barros <nbarros@uolinc.com>
2019-03-14 11:31:35 -0300

    Load Image

--- app/src/main/java/com/example/estudoandroid/MainActivity.kt
+++ app/src/main/java/com/example/estudoandroid/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.estudoandroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,33 +14,58 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        val kotlin = ProgrammingLanguage(
-                R.drawable.ic_developer_board,
-                "Kotlin",
-                2010,
-                "Kotlin Description"
-        )
-
-        val items = listOf(kotlin, kotlin)
-
-        val adapter = ProgrammingLanguageAdapter(items) {
-            longToast(it.title)
+        btLoadImage.setOnClickListener{
+            loadImage()
         }
-        recyclerView.adapter = adapter
+    }
 
-        alert("Message", "Title") {
-            yesButton {
-                toast("OK")
-            }
-            cancelButton {
-                toast("Cancel")
-            }
-        }.show()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
-//        doAsync {
-//
-//        }
+        if(requestCode === SELECT_PICTURE) {
+            if(resultCode == RESULT_OK) {
+
+                if(data?.extras != null) {
+                    val bitmap = data.extras?.get("data") as Bitmap
+                    ivMain.setImageBitmap(bitmap)
+                } else if (data?.data != null) {
+                    Picasso.get().load(data.data).into(ivMain)
+                }
+
+            }
+        }
+    }
+
+
+    private fun loadImage() {
+        //Intent sao controladas pelo Android.
+        //ACTION_GET_CONTENT abre os docs
+        val pickIntent = Intent(Intent.ACTION_GET_CONTENT)
+
+        //nos mais novos nao tem
+        pickIntent.type = "image/*"
+
+
+        val pickGalleryIntent = Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        val chooserIntent = Intent.createChooser(pickIntent, "Select or take a image")
+        chooserIntent.putExtra(
+                Intent.EXTRA_INITIAL_INTENTS,
+                arrayOf(pickGalleryIntent, takePictureIntent))
+
+        //Para evitar erro - ActivityNotFoundException
+        if(chooserIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(chooserIntent, SELECT_PICTURE)
+        }
+    }
+
+
+
+
+    companion object {
+        private const val SELECT_PICTURE = 1
     }
 }
