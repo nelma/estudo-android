3201b5e384c969e612725bb4f21d9314705533fd
Nelma Barros <nbarros@uolinc.com>
2019-04-07 11:22:18 -0300

    Aplicando estilo delete individual e alterando estilo do textview

--- app/src/main/java/com/example/estudoandroid/view/BasicActivity.kt
+++ app/src/main/java/com/example/estudoandroid/view/BasicActivity.kt
@@ -2,6 +2,10 @@ package com.example.estudoandroid.view
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -9,6 +13,7 @@ import android.view.MotionEvent
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -35,6 +40,9 @@ class BasicActivity : AppCompatActivity() {
     companion object {
         private const val NEW_WORD_REQUEST_CODE: Int = 1
     }
+
+    private var swipeBackground: ColorDrawable = ColorDrawable(Color.parseColor("#FF0000"))
+    private lateinit var deleteIcon: Drawable
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
@@ -73,6 +81,8 @@ class BasicActivity : AppCompatActivity() {
         recyclerView.setHasFixedSize(true)
         recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
 
+        deleteIcon = ContextCompat.getDrawable(this, R.drawable.ic_delete)!!
+
         val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
                 0,
                 ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
@@ -81,10 +91,47 @@ class BasicActivity : AppCompatActivity() {
             }
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, position: Int) {
-                wordViewModel.delete(adapter.getItem(viewHolder.adapterPosition))
-                longToast("Word deleted!")
+//                alert ("Do you wish this item?") {
+//                    title = "Warning"
+//                    yesButton {
+                        wordViewModel.delete(adapter.getItem(viewHolder.adapterPosition))
+                        longToast("Word deleted!")
+//                    }
+//                    noButton {
+//                        longToast("No")
+//                    }
+//                }.show()
             }
 
+            override fun onChildDraw(c: Canvas,
+                                     recyclerView: RecyclerView,
+                                     viewHolder: RecyclerView.ViewHolder,
+                                     dX: Float,
+                                     dY: Float,
+                                     actionState: Int,
+                                     isCurrentlyActive: Boolean) {
+
+                val itemView = viewHolder.itemView
+                val iconMargin = (itemView.height - deleteIcon.intrinsicHeight) / 2
+
+                if(dX > 0) {
+                    swipeBackground.setBounds(itemView.left, itemView.top, dX.toInt(), itemView.bottom)
+                    deleteIcon.setBounds(itemView.left + iconMargin, itemView.top + iconMargin,
+                            itemView.left + iconMargin + deleteIcon.intrinsicWidth,
+                            itemView.bottom - iconMargin)
+
+                } else {
+                    swipeBackground.setBounds(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+                    deleteIcon.setBounds(itemView.right - iconMargin - deleteIcon.intrinsicWidth,
+                            itemView.top + iconMargin,
+                            itemView.right - iconMargin,
+                            itemView.bottom - iconMargin)
+                }
+
+                swipeBackground.draw(c)
+                deleteIcon.draw(c)
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            }
         }
 
         val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
